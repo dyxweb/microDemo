b62704025fab6ee86f4e2784dfb9aba1cba19025
dyxweb <13483409091@163.com>
2023-05-12 16:47:03 +0800

    feat: 主应用支持微前端

--- main/src/index.tsx
+++ main/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { registerMicroApps, start } from 'qiankun';
 import { ConfigProvider } from 'antd';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import zhCN from 'antd/lib/locale/zh_CN';
@@ -15,6 +16,8 @@ ReactDOM.render(
   <ConfigProvider locale={zhCN}>
       <BrowserRouter>
         <PageLoyout>
+          {/* 微应用挂载节点 */}
+          <div id="microContainer" />
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/exer' component={Exer} />
@@ -24,3 +27,20 @@ ReactDOM.render(
   </ConfigProvider>,
   document.getElementById('root')
 );
+
+registerMicroApps([
+  {
+    name: 'micro-react',
+    entry: '//localhost:8001',
+    container: '#microContainer',
+    activeRule: '/micro-react',
+  },
+  {
+    name: 'micro-vue',
+    entry: '//localhost:8002',
+    container: '#microContainer',
+    activeRule: '/micro-vue',
+  },
+]);
+// 启动 qiankun
+start();
