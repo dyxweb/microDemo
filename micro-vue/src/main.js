fd7c712005edf55fbc7232261b6ab9917253cb9c
dyxweb <13483409091@163.com>
2023-05-12 17:46:35 +0800

    feat: vue子应用支持微前端

--- micro-vue/src/main.js
+++ micro-vue/src/main.js
@@ -4,13 +4,36 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue';
 import routes from './routes.js';
+import './public-path';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
-const app = createApp(App);
+let instance = null;
+function render(props = {}) {
+  if (instance) return;
+  const { container } = props;
+  const router = createRouter({
+    history: createWebHistory(window.__POWERED_BY_QIANKUN__ ? '/micro-vue/' : '/'),
+    routes,
+  })
+  instance = createApp(App);
 
-app.use(router);
-app.use(ElementPlus);
-app.mount('#app');
+  instance.use(router);
+  instance.use(ElementPlus);
+  instance.mount(container ? container.querySelector('#app') : '#app');
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped');
+}
+export async function mount(props) {
+  console.log('[vue] props from main framework', props);
+  render(props);
+}
+export async function unmount() {
+  instance.$destroy?.();
+  instance = null;
+}
